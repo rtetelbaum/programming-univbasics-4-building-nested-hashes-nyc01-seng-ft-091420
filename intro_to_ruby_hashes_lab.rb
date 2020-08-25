619c1bd80a7fdae5cf42f79a744b7ba7846f6a16
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-08-25 21:02:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,10 @@ def monopoly_with_second_tier
     monopoly = {
     :railroads => {
       :pieces => 4,
-      rent_in_dollars: {
+      :rent_in_dollars => {
+        
+      }
+      :names => {
         
       }
     }
