3d6b2c25b3ed873979b0841465dc677ce35fd28e
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-08-25 21:05:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,10 +38,10 @@ def monopoly_with_third_tier
     :railroads => {
       :pieces => 4,
       :rent_in_dollars => {
-        
+        :one_piece_owned => 25
       },
       :names => {
-        :one_piece_owned => 25
+        
       }
     }
   }
